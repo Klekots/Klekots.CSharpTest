38868f32a34482c4ba915032959c5258b8172137
Klekots <32235474+Klekots@users.noreply.github.com>
2020-01-16 15:05:29 +0200

    Second Commit

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Klekots.SecondApp
         }
         static void Main(string[] args)
         {
-            List<uint> IndividualSpeed = new List<uint>();
+            List<Worker> Workers = new List<Worker>();
 
             Console.WriteLine("Enter Images Count");
             uint PictureCount = Conv();
@@ -19,42 +19,79 @@ namespace Klekots.SecondApp
             Console.WriteLine("Enter People Count");
             uint PeopleCount = Conv();
 
-            for(var i = 0; i < PeopleCount; i++)
+            for (var i = 0; i < PeopleCount; i++)
             {
-                Console.WriteLine($"Enter people {i+1} speed");
-                IndividualSpeed.Add(Conv());
+                Console.WriteLine($"Enter people {i + 1} speed");
+                Workers.Add(new Worker(Conv(), 0));
             }
 
-            float FirstStep(uint peopleCount,List<uint> individualSpeed)
+            float FirstStep(uint peopleCount, List<Worker> workers)
             {
                 float allPeopleWork = 0;
-                for(var i = 0; i < peopleCount; i++)
+                for (var i = 0; i < peopleCount; i++)
                 {
-                    allPeopleWork += ((float)1 / individualSpeed[i]);
+                    allPeopleWork += ((float)1 / workers[i].Speed);
                 }
 
                 return allPeopleWork;
             }
 
-            uint time = (uint)(PictureCount / FirstStep(PeopleCount, IndividualSpeed));
+            uint time = (uint)(PictureCount / FirstStep(PeopleCount, Workers));
 
-            uint SecondStep(uint peopleCount, List<uint> individualSpeed)
+            uint SecondStep(uint peopleCount, List<Worker> workers)
             {
                 float sum = 0;
 
                 for (var i = 0; i < peopleCount; i++)
                 {
-                    sum += (time / individualSpeed[i]);
+                    sum += (time / workers[i].Speed);
                 }
                 return (uint)sum;
             }
 
-            while (SecondStep(PeopleCount, IndividualSpeed) < PictureCount)
+            while (SecondStep(PeopleCount, Workers) < PictureCount)
             {
                 time++;
             }
 
-            Console.WriteLine(time);
+            for (var i = 0; i < PeopleCount; i++)
+            {
+                Workers[i].PhotoCheckedCount = time / Workers[i].Speed;
+            }
+
+            Workers.Sort((Worker first, Worker second) =>
+            {
+                if (first.PhotoCheckedCount > second.PhotoCheckedCount)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            });
+
+            uint CheckedSum(uint peopleCount, List<Worker> workers)
+            {
+                uint sum = 0;
+                for (var i = 0; i < peopleCount; i++)
+                {
+                    sum += workers[i].PhotoCheckedCount;
+                }
+                return sum;
+            }
+
+            for(var i = (int)PeopleCount-1; CheckedSum(PeopleCount, Workers) > PictureCount; i--)
+            {
+                Workers[i].PhotoCheckedCount--;
+            }
+
+            foreach (Worker people in Workers)
+            {
+                Console.WriteLine($"Worker id: {people.WorkerID} checked {people.PhotoCheckedCount} photos");
+            }
+
+            Console.WriteLine($"Time needed to complete task :  {time}");
             Console.ReadLine();
         }
     }
---
+++ Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Klekots.SecondApp
+{
+    class Worker
+    {
+        static uint WorkersCount { get; set; } = 1;
+        public uint Speed { get; set; }
+        public uint PhotoCheckedCount { get; set; }
+        public uint WorkerID { get; }
+        public Worker(uint speed, uint photoChecked)
+        {
+            Speed = speed;
+            PhotoCheckedCount = photoChecked;
+            WorkerID = WorkersCount;
+            WorkersCount++;
+        }
+    }
+}
